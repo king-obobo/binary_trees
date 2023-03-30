2e7f3492336f8e78bffea4b53988dff15f164424
king-obobo <oboboebuka1@gmail.com>
2023-03-30 14:45:18 -0700

    Task 134 again

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -8,23 +8,24 @@
  *
  * Return: Sorted in descending order
  */
-
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int i, *a = NULL;
+	int *array;
+	int extract, i = 0;
+	size_t heap_size;
 
-	if (!heap || !size)
+	if (!heap)
 		return (NULL);
-
-	*size = tree_size(heap) + 1;
-
-	a = malloc(sizeof(int) * (*size));
-
-	if (!a)
+	heap_size = binary_tree_size(heap);
+	*size = heap_size;
+	array = malloc(heap_size * sizeof(int));
+	if (!array)
 		return (NULL);
-
-	for (i = 0; heap; i++)
-		a[i] = heap_extract(&heap);
-
-	return (a);
+	while (heap)
+	{
+		extract = heap_extract(&heap);
+		array[i] = extract;
+		i++;
+	}
+	return (array);
 }
